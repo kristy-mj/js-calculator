689837155910d9924cf6a015025e9f4f0a874bbb
Kristy <johnstonekristy@gmail.com>
2016-05-04 17:17:55 +1200

    Add 0 to initial calculator display

--- js/calculator.js
+++ js/calculator.js
@@ -3,44 +3,44 @@ $(document).ready(function(){
         if (num.length > 9) {
             displaySum.text(num.substr(num.length-9,9));
             if (num.length > 15) {
-                num = "";
-                displaySum.text("error");
+                num = '';
+                displaySum.text('error');
             }
         } 
-    
   }
 });
 
 
-var num = "";
-var newNum = "";
-var operator = "";
+var num = '0';
+var newNum = '';
+var operator = '';
 var displaySum = $('#display');
-
+displaySum.text(num);
 
 $('.buttons button').on('click', function(event) {
   $('#display').append(event.currentTarget.innerHTML);
   
-  if ($(this).attr("class") === 'clear') {
-    num = "";
-    displaySum.text(" ");
+  if ($(this).attr('class') === 'clear') {
+    num = '';
+    displaySum.text('');
   }
-  else if ($(this).attr("class") === "clearall") {
-      displaySum.text(" ");
-      newNum = "";
+  else if ($(this).attr('class') === 'clearall') {
+      displaySum.text('');
+      num = '';
+      newNum = '';
   }
-  else if ($(this).attr("class") === 'number') {
+  else if ($(this).attr('class') === 'number') {
     num += $(this).text();
     displaySum.text(num);
     // testNumLength(num);
   }
-  else if ($(this).attr("class") === 'operator') {
+  else if ($(this).attr('class') === 'operator') {
     operator = $(this).text();
     displaySum.text(operator);
     newNum = num;
-    num = "";
+    num = '';
   }
-  else if ($(this).attr("class") === 'equals') {
+  else if ($(this).attr('class') === 'equals') {
     
     if (operator === '+') {
       num = (parseInt(newNum, 10) + parseInt(num, 10)).toString(10);
@@ -54,7 +54,6 @@ $('.buttons button').on('click', function(event) {
     else if (operator === '/') {
       num = (parseInt(newNum, 10) / parseInt(num, 10)).toString(10);
     }
-    // operator = '';
     displaySum.text(num);
     number = '';
     newNum = '';
